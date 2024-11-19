1176075d85789677e5bea1efd1236352c0ed689f
Code2With-Pratik <work.pratik1204@gmail.com>
2024-11-19 21:27:29 +0530

    username is already regesterd comment added

--- login_register.php
+++ login_register.php
@@ -9,7 +9,33 @@
 
     if($result)
     {
+      if(mysqli_num_rows($result)>0)
+      {
+        $result_fetch=mysqli_fetch_assoc($result);
+        if($result_fetch['username']==$_POST['username'])
+        {
+            # username is already regesterd
+          echo"
+            <script>
+                alert('$result_fetch[username] - Username already taken');
+                window.location.href='index.php';
+            </script>
+          "; 
+        }
+      }
+      else
+      {
+        echo"
+            <script>
+                alert('Cannot Run Query');
+                window.location.href='index.php';
+            </script>
+        ";
+      }
+      else
+      {
 
+      }
     }
     else
     {
@@ -21,5 +47,5 @@
       ";
     }
   }
-  
+
 ?>
